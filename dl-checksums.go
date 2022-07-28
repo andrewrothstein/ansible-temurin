83e4df7a9fc89d5fc0858111e2bb36f2744707db
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-07-28 09:23:47 -0400

    8u342b07, 18.0.2+9

--- dl-checksums.go
+++ dl-checksums.go
@@ -171,6 +171,7 @@ func main() {
 		{Major: 8, Minor: 312, Patch: "0", BVer: "07"},
 		{Major: 8, Minor: 322, Patch: "0", BVer: "06"},
 		{Major: 8, Minor: 332, Patch: "0", BVer: "09"},
+		{Major: 8, Minor: 342, Patch: "0", BVer: "07"},
 		{Major: 11, Minor: 0, Patch: "13", BVer: "8"},
 		{Major: 11, Minor: 0, Patch: "14.1", BVer: "1"},
 		{Major: 11, Minor: 0, Patch: "15", BVer: "10"},
@@ -181,6 +182,7 @@ func main() {
 		{Major: 17, Minor: 0, Patch: "3", BVer: "7"},
 		{Major: 17, Minor: 0, Patch: "4", BVer: "8"},
 		{Major: 18, Minor: 0, Patch: "1", BVer: "10"},
+		{Major: 18, Minor: 0, Patch: "2", BVer: "9"},
 	}
 	dlall(1, &params, versions, platforms)
 }
