b72aa160ad9cb81385716d54897df59bb1ee76bc
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-04-07 17:19:10 +0000

    8u442b06 11.0.26_4 17.0.14_7 21.0.6_7 23.0.2_7

--- dl-checksums.go
+++ dl-checksums.go
@@ -201,6 +201,7 @@ func main() {
 		{Major: 8, Minor: 412, Patch: "0", BVer: "08"},
 		{Major: 8, Minor: 422, Patch: "0", BVer: "05"},
 		{Major: 8, Minor: 432, Patch: "0", BVer: "06"},
+		{Major: 8, Minor: 442, Patch: "0", BVer: "06"},
 		{Major: 11, Minor: 0, Patch: "13", BVer: "8"},
 		{Major: 11, Minor: 0, Patch: "14.1", BVer: "1"},
 		{Major: 11, Minor: 0, Patch: "15", BVer: "10"},
@@ -215,6 +216,7 @@ func main() {
 		{Major: 11, Minor: 0, Patch: "23", BVer: "9"},
 		{Major: 11, Minor: 0, Patch: "24", BVer: "8"},
 		{Major: 11, Minor: 0, Patch: "25", BVer: "9"},
+		{Major: 11, Minor: 0, Patch: "26", BVer: "4"},
 		{Major: 16, Minor: 0, Patch: "2", BVer: "7"},
 		{Major: 17, Minor: 0, Patch: "1", BVer: "12"},
 		{Major: 17, Minor: 0, Patch: "2", BVer: "8"},
@@ -230,6 +232,7 @@ func main() {
 		{Major: 17, Minor: 0, Patch: "11", BVer: "9"},
 		{Major: 17, Minor: 0, Patch: "12", BVer: "7"},
 		{Major: 17, Minor: 0, Patch: "13", BVer: "11"},
+		{Major: 17, Minor: 0, Patch: "14", BVer: "7"},
 		{Major: 18, Minor: 0, Patch: "1", BVer: "10"},
 		{Major: 18, Minor: 0, Patch: "2", BVer: "9"},
 		{Major: 18, Minor: 0, Patch: "2.1", BVer: "1"},
@@ -243,8 +246,10 @@ func main() {
 		{Major: 21, Minor: 0, Patch: "3", BVer: "9"},
 		{Major: 21, Minor: 0, Patch: "4", BVer: "7"},
 		{Major: 21, Minor: 0, Patch: "5", BVer: "11"},
+		{Major: 21, Minor: 0, Patch: "6", BVer: "7"},
 		{Major: 22, Minor: 0, Patch: "1", BVer: "8"},
 		{Major: 22, Minor: 0, Patch: "2", BVer: "9"},
+		{Major: 23, Minor: 0, Patch: "2", BVer: "7"},
 	}
 	dlall(1, &params, versions, platforms)
 }
